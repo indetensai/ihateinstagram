86bbc03f4c987f244454d62f608ff247a382483b
indetensai <artyomvagin12@gmail.com>
2023-01-22 02:12:00 +0200

    previous project copypaste
    happy already(working)

--- main.go
+++ main.go
@@ -27,6 +27,43 @@ func error_check(err error, status string) {
 	}
 }
 
+func login_handler(c *fiber.Ctx) error {
+	hash := sha256.New()
+	hash.Write([]byte(c.FormValue("password")))
+	pede := hash.Sum(nil)
+	var b string
+	var id uuid.UUID
+	letter := new(register_result)
+	err := con.QueryRow(context.Background(), "SELECT password,user_id FROM app_user WHERE username=$1", c.FormValue("username")).Scan(&b, &id)
+	if err != nil {
+		fmt.Println(err)
+		c.Write([]byte("username not found"))
+		c.Status(fiber.StatusNotFound)
+		return err
+	}
+
+	error_check(err, "scanning failed")
+
+	if b == fmt.Sprintf("%x", pede) {
+		letter.Success = true
+		letter.Note = "sucessfull login!! congratz."
+		_, err := con.Exec(context.Background(), "INSERT INTO session (user_id) VALUES ($1)", id)
+		error_check(err, "creating session failed")
+		j, err := json.Marshal(letter)
+		error_check(err, "response writing failed")
+		c.Write(j)
+		c.Status(fiber.StatusOK)
+	} else {
+		letter.Success = false
+		letter.Note = "password is wrong, ure brainless"
+		j, err := json.Marshal(letter)
+		error_check(err, "response writing failed")
+		c.Write(j)
+		c.Status(fiber.StatusNonAuthoritativeInformation)
+	}
+	return err
+}
+
 func register_handler(c *fiber.Ctx) error {
 	id := uuid.New()
 	hash := sha256.New()
@@ -63,11 +100,18 @@ func baza() {
 		username VARCHAR(50) UNIQUE NOT NULL,
 		password VARCHAR(64) NOT NULL
 		);`)
+	con.Exec(context.Background(),
+		`CREATE TABLE IF NOT EXISTS session(
+		session_id UUID PRIMARY KEY NOT NULL UNIQUE DEFAULT gen_random_uuid() ,
+		user_id uuid REFERENCES app_user(user_id) ON DELETE CASCADE,
+		created_at TIMESTAMP NOT NULL DEFAULT now()	
+		); `)
 }
 func main() {
 	godotenv.Load(".env")
 	baza()
 	app := fiber.New()
 	app.Post("/user/register", register_handler)
+	app.Post("/user/login", login_handler)
 	app.Listen(":8080")
 }
