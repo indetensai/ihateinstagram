c1fb423fd81f7dbbdcdb58002ac07949f8a5dcb2
indetensai <artyomvagin12@gmail.com>
2023-01-23 19:30:44 +0200

    tables for post feature

--- main.go
+++ main.go
@@ -26,16 +26,29 @@ func based() {
 	con.Ping(context.Background())
 	con.Exec(context.Background(),
 		`CREATE TABLE IF NOT EXISTS app_user(
-		user_id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+		user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 		username VARCHAR(50) UNIQUE NOT NULL,
 		password VARCHAR(64) NOT NULL
 		);`)
 	con.Exec(context.Background(),
 		`CREATE TABLE IF NOT EXISTS session(
-		session_id UUID PRIMARY KEY NOT NULL UNIQUE DEFAULT gen_random_uuid() ,
-		user_id uuid REFERENCES app_user(user_id) ON DELETE CASCADE,
+		session_id UUID PRIMARY KEY UNIQUE DEFAULT gen_random_uuid(),
+		user_id UUID REFERENCES app_user(user_id) ON DELETE CASCADE,
 		created_at TIMESTAMP NOT NULL DEFAULT now()	
-		); `)
+		);`)
+	con.Exec(context.Background(),
+		`CREATE TABLE IF NOT EXISTS post(
+		post_id UUID PRIMARY KEY UNIQUE DEFAULT gen_random_uuid(),
+		user_id UUID NOT NULL REFERENCES app_user(user_id) ON DELETE CASCADE,
+		description TEXT
+		);`)
+	con.Exec(context.Background(),
+		`CREATE TABLE IF NOT EXISTS image(
+		image_id UUID PRIMARY KEY UNIQUE DEFAULT gen_random_uuid(),
+		user_id UUID NOT NULL REFERENCES app_user(user_id) ON DELETE CASCADE,
+		post_id UUID REFERENCES post(post_id) ON DELETE SET NULL,
+		content BYTEA 
+		);`)
 }
 func main() {
 	godotenv.Load(".env")
