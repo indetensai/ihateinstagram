77853a28db39ef9605ca1b72893dce1f6c3809d7
indetensai <artyomvagin12@gmail.com>
2023-01-23 20:17:34 +0200

    database separate file

--- main.go
+++ main.go
@@ -1,58 +1,13 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"os"
-
 	"github.com/gofiber/fiber/v2"
-	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
 )
 
-var con *pgx.Conn
-
-func error_check(err error, status string) {
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s - %v\n", status, err)
-		os.Exit(1)
-	}
-}
-
-func based() {
-	var err error
-	con, err = pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
-	error_check(err, "connection to database failed")
-	con.Ping(context.Background())
-	con.Exec(context.Background(),
-		`CREATE TABLE IF NOT EXISTS app_user(
-		user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-		username VARCHAR(50) UNIQUE NOT NULL,
-		password VARCHAR(64) NOT NULL
-		);`)
-	con.Exec(context.Background(),
-		`CREATE TABLE IF NOT EXISTS session(
-		session_id UUID PRIMARY KEY UNIQUE DEFAULT gen_random_uuid(),
-		user_id UUID REFERENCES app_user(user_id) ON DELETE CASCADE,
-		created_at TIMESTAMP NOT NULL DEFAULT now()	
-		);`)
-	con.Exec(context.Background(),
-		`CREATE TABLE IF NOT EXISTS post(
-		post_id UUID PRIMARY KEY UNIQUE DEFAULT gen_random_uuid(),
-		user_id UUID NOT NULL REFERENCES app_user(user_id) ON DELETE CASCADE,
-		description TEXT
-		);`)
-	con.Exec(context.Background(),
-		`CREATE TABLE IF NOT EXISTS image(
-		image_id UUID PRIMARY KEY UNIQUE DEFAULT gen_random_uuid(),
-		user_id UUID NOT NULL REFERENCES app_user(user_id) ON DELETE CASCADE,
-		post_id UUID REFERENCES post(post_id) ON DELETE SET NULL,
-		content BYTEA 
-		);`)
-}
 func main() {
 	godotenv.Load(".env")
-	based()
+	database_connection()
 	app := fiber.New()
 	app.Post("/user/register", register_handler)
 	app.Post("/user/login", login_handler)
